c2fd50367f40aac0c42a3cbff93331f72490d837
HrDelwar <hrdelwar75@gmail.com>
2021-04-08 12:39:22 +0600

    changes route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <div style={{paddingBottom:'7rem'}} >
             <nav className="navbar navbar-expand-lg fixed-top   bg-light custom-nav-bg">
-                <a className="navbar-brand logo" href="#"><img src={logo} alt="Logo" />
+                <a className="navbar-brand logo" href="/"><img src={logo} alt="Logo" />
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} />
@@ -17,13 +17,13 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Players</a>
+                            <a className="nav-link" href="/">Players</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Blog</a>
+                            <a className="nav-link" href="/">Blog</a>
                         </li>
                     </ul>
                 </div>
